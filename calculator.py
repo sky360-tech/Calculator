b1e3f8b4d2b77602aef20e4beef57af0f518c025
A S M Tahmir Hasan <134987771+sky360-tech@users.noreply.github.com>
2024-01-13 18:52:29 -0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,37 @@
+num1 = input()
+num2 = input()
+
+max = 9999999
+min = 0
+if num1 > max:
+    print("Syntax Error")
+    exit()
+if num1 > min:
+   Addition= num1 + num2
+   print(Addition) 
+   Subtraction= num1 - num2
+   print(Subtraction)
+   Multiplication= num1 * num2
+   print(Multiplication)
+   Division= num1 / num2
+   print(Division)
+   Modulo= num1 % num2
+   print(Modulo)
+   Exponent= num1 ** num2
+   print(Exponent)
+   Floor_Division= num1 // num2
+   print(Floor_Division)
+   Remainder= num1 % num2
+   print(Remainder)
+   Square_Root= num1 ** 0.5
+   print(Square_Root)
+   Square= num1 ** 2
+   print(Square)
+   Cube= num1 ** 3
+   print(Cube)
+   Cube_Root= num1 ** (1/3)
+   print(Cube_Root)
+   Factorial= num1
+   print(Factorial)
+exit()
+
